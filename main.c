d8a3dc6400f49dee9f1a917145d84aaf9a4e8032
codistro <rajan191455@gmail.com>
2017-04-01 10:33:47 +0530

    added readCommand function

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 1
+char *readCommand(){
+	int bufferSize = SIZE;
+	char *p = malloc(sizeof(char) * bufferSize);
+	int position = 0;
+	if(!p){
+		printf("ERROR_ALLOCATING_SPACE\n");
+		exit(1);
+	}
+	do{
+		int c = getchar();
+		p[position++] = c;
+		if(c == EOF || c == '\n'){
+			p[position] = '\0';
+			return p;
+		}
+		if(position >= SIZE){
+			bufferSize = bufferSize * 2;
+			p = realloc(p,bufferSize);
+			if(!p){
+				printf("ERROR_ALLOCATING_SPACE\n");
+				exit(1);
+			}
+		}		
+	}while(1);
+}
+int main(int argc,char *argv[]){
+	char *arr = readCommand();
+	printf("%s",arr);
+	return 0;
+}
